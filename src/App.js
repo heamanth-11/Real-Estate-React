a404f1c5b594a055785e54c659f63de21e653d27
HariNivas <harinivasanm2001@gmail.com>
2021-12-06 21:16:56 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+     <Header/>
     </div>
   );
 }
