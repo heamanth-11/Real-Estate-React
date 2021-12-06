ba26b4e501d6bb09b8a3a180f799958c4c5fab1a
HariNivas <harinivasanm2001@gmail.com>
2021-12-06 21:35:58 +0530

    Completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+function Footer()
+{
+    return(
+        <>
+      <footer className=" text-white pt-5 pb-4">
+
+<div className="container text-center text-md-left">
+
+    <div className="row text-center text-md-left">
+
+        <div className="col-md-5 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold head">The More</h5>
+            <div>
+            <p>
+                <i className="fas fa-home mr-3"></i>New York, NY 2333, US
+            </p>
+            <p>
+                <i className="fas fa-envelope mr-3"></i>theproviders98@gmail.com
+            </p>
+            <p>
+                <i className="fas fa-phone mr-3"></i>+92 3162859445
+            </p>
+            <p>
+                <i className="fas fa-print	 mr-3"></i>+01 335 633 77
+            </p>
+        </div>
+            
+        </div>
+
+        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold head">Navigations</h5>
+        <p>
+            <a href="/" className="text-white" > About Us</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > Checkout</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > FAQs Page</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > Contact</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > Blog</a>
+        </p>
+
+        </div>
+
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold head">Useful links</h5>
+        <p>
+            <a href="/" className="text-white" > Rent</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > Buy</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" >Sale</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > Help</a>
+        </p>
+        </div>
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold head">Useful links</h5>
+        <p>
+            <a href="/" className="text-white" > Your Account</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > Profile</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" >Properties</a>
+        </p>
+        <p>
+            <a href="/" className="text-white" > Favorites</a>
+        </p>
+        </div>
+
+     
+        
+    </div>
+
+    <hr className="mb-4"/>
+
+    <div className="row align-items-center">
+
+        <div className="col-md-7 col-lg-8">
+            <p>	Copyright ©2020 All rights reserved by:
+                <a href="/" >
+                    <strong className="head">The More</strong>
+                </a></p>
+            
+        </div>
+
+        <div className="col-md-5 col-lg-4">
+            <div className="text-center text-md-right">
+
+                <ul className="list-unstyled list-inline">
+                    <li className="list-inline-item">
+                        <a href="/" className="btn-floating btn-sm text-white icons" ><i className="fab fa-facebook"></i></a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href="/" className="btn-floating btn-sm text-white icons" ><i className="fab fa-twitter"></i></a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href="/" className="btn-floating btn-sm text-white icons" ><i className="fab fa-google-plus"></i></a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href="/" className="btn-floating btn-sm text-white icons" ><i className="fab fa-linkedin-in"></i></a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href="/" className="btn-floating btn-sm text-white icons" ><i className="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+
+</div>
+
+</footer>
+        </>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Store.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Store()
+{
+    return(
+        <>
+        <section className="section cta-section">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
+            <h2>Download App Free App For Android And IPhone</h2>
+          </div>
+          <div className="col-md-5 text-center text-md-end">
+            <p><a href="/" className="btn d-inline-flex align-items-center"><i className="fab fa-apple btnn"></i><span>App store</span></a> <a href="/" className="btn d-inline-flex align-items-center "><i class="fas fa-play btnn"></i><span>Google play</span></a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+        </>
+    );
+}
+
+export default Store;
+
+
+
