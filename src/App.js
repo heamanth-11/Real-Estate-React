e8860b963ee1cbf20740603681803e9a712dcae3
sanjaidebuger <nsksanjai.com>
2021-12-09 20:23:26 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Store from "./Components/Store"
 import RecentProperty from "./Components/RecentProperty";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Home />
      <RecentProperty />
      <Store />
      <Footer />
